292bd6a702befbac1c24ac36e5e376f295ba4fb3
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-07-26 01:13:02 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  var result = (function() {
-    var task = false;
-    var task2 = true;
+  let task = false;
+  let task2 = true;
 
-    if (trueOrFalse) {
-      task = true;
-      task2 = false;
-    }
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
-    return [task, task2];
-  })();
-
-  return result;
+  return [task, task2];
 }
