cb3fe0cb7712f65058bd2ecc493a8e49db88a8ff
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-07-28 10:49:11 +0100

    Create 1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const classroom1 = new ClassRoom(19);
+  const classroom2 = new ClassRoom(20);
+  const classroom3 = new ClassRoom(34);
+
+  return [classroom1, classroom2, classroom3];
+}
+
+const classrooms = initializeRooms();
+console.log(classrooms); // Output: [ClassRoom { _maxStudentsSize: 19 }, ClassRoom { _maxStudentsSize: 20 }, ClassRoom { _maxStudentsSize: 34 }]
