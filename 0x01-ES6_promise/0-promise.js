86874f96ca1be756fa2a0c04eaee2592c465f949
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-08-14 10:56:46 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API call
-    setTimeout(() => {
-      const success = true; // Change this based on your API response
-      if (success) {
-        const responseData = {
-          status: 200,
-          data: 'API response data'
-        };
-        resolve(responseData);
-      } else {
-        reject(new Error('API request failed'));
-      }
-    }, 1000); // Simulating a 1-second delay for the API call
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
