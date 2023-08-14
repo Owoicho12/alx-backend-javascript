3b160dcb2379342ba3da4b2dbc90c516335f40f4
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-08-14 12:38:10 +0100

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@
  * Changes the quantity of unique grocery items to 100.
  * @param {Map<String, number>} map - A map of the name of a
  * grocery and its quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Olatunde Oloyede <https://github.com/sirtuns4real>
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
