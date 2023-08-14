8d8ed0cb68939a01b65584151566f18eb953af45
Olatunde, J. Oloyede <olatundeoloyede41@yahoo.com>
2023-08-14 12:31:49 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Oloyede Olatunde <https://github.com/sirtuns4real>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
